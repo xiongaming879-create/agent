4b82b48e3498c40b067b63e67b92a7ccaaaa8893
xiongaming879-create <xiongaming879@gmail.com>
2026-06-25 15:22:58 +0800

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,1 +1,21 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+      '/avatars': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+    historyApiFallback: true,
+  },
+})
